05307f3d0e196fe1e6c8b2d88936742e512de473
Ēriks Vansovičs <A240150EV@rvt.lv>
2026-05-21 10:48:44 +0300

    Launcher

---
+++ java-oop-23DP4-2EVans-main/src/main/java/rvt/Swing_01/Launcher.java
@@ -0,0 +1,15 @@
+package rvt.Swing_01;
+import javax.swing.SwingUtilities;
+
+
+public class Launcher {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainWindow main = new MainWindow();
+                main.show(); // <-- arī šeit pietrūka ;
+            }
+        });
+    }
+}
