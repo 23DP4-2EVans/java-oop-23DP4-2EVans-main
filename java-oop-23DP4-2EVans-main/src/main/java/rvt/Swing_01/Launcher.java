891bbc61042cf9660eff8e114c5ab9d6c0ce8dc5
Ēriks Vansovičs <A240150EV@rvt.lv>
2026-05-21 10:53:04 +0300

    Launcher

--- java-oop-23DP4-2EVans-main/src/main/java/rvt/Swing_01/Launcher.java
+++ java-oop-23DP4-2EVans-main/src/main/java/rvt/Swing_01/Launcher.java
@@ -1,14 +1,27 @@
-package rvt.Swing_01;
+package rvt.Swing_01; // Norāda, kurā paketē (mapē) atrodas šī klase
+
+// Importē SwingUtilities klasi, kas palīdz palaist GUI (logu) pareizi
 import javax.swing.SwingUtilities;
 
-
+// Galvenā klase, no kuras sākas programma
 public class Launcher {
+
+    // Galvenā metode (ieejas punkts programmā)
     public static void main(String[] args) {
+
+        // Šī metode nodrošina, ka GUI tiek palaists uz pareizā pavediena (Event Dispatch Thread)
+        // Swing bibliotēka prasa, lai visi GUI elementi tiktu veidoti šajā pavedienā
         SwingUtilities.invokeLater(new Runnable() {
+
+            // Šī metode tiks izsaukta, kad programma startēs GUI
             @Override
             public void run() {
+
+                // Izveido jaunu logu (MainWindow objektu)
                 MainWindow main = new MainWindow();
-                main.show(); // <-- arī šeit pietrūka ;
+
+                // Parāda logu uz ekrāna
+                main.show();
             }
         });
     }
